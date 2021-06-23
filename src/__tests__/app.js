a248518c9ae6e6cce069377284059242fdc8e89b
Rob Taylor <manix84@gmail.com>
2021-06-23 10:05:25 +0100

    Switching to SemiStandard. Oh god, I missed semi-colons! 😢

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,30 +1,30 @@
-const app = require('../app')
-const request = require('supertest')
+const app = require('../app');
+const request = require('supertest');
 
 describe('Test basic endpoints', () => {
   // GET /servers/<UID> -> 200 | 404 | 5XX
   test('GET /servers/<UID>', async () => {
     await request(app).get('/servers/<UID>')
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   // GET /servers/<UID>/users 200 | 403 | 404
   test('GET /servers/<UID>/users', async () => {
     await request(app).get('/servers/<UID>/users')
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   // GET /servers/<UID>/users/<UID> 200 | 403 | 404
   test('GET /servers/<UID>/users/<UID>', async () => {
     await request(app).get('/servers/<UID>/users/<UID>')
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   // POST /servers/<UID>/users/<UID>/mute 200 | 403 | 404
   test('POST /servers/<UID>/users/<UID>/<COMMAND>', async () => {
     await request(app).post('/servers/<UID>/users/<UID>/<COMMAND>')
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   // POST /servers/<UID>/users/<UID>/mute 200 | 403 | 404
   test('POST /servers/<UID>/users/<UID>/<VALID COMMANDS>', async () => {
@@ -32,30 +32,30 @@ describe('Test basic endpoints', () => {
       .expect(200)
       .then(response =>
         response.body.command === 'mute'
-      )
+      );
 
     await request(app).post('/servers/<UID>/users/<UID>/unmute')
       .expect(200)
       .then(response =>
         response.body.command === 'unmute'
-      )
+      );
 
     await request(app).post('/servers/<UID>/users/<UID>/deafen')
       .expect(200)
       .then(response =>
         response.body.command === 'deafen'
-      )
+      );
 
     await request(app).post('/servers/<UID>/users/<UID>/undeafen')
       .expect(200)
       .then(response =>
         response.body.command === 'undeafen'
-      )
+      );
 
     await request(app).post('/servers/<UID>/users/<UID>/invalid')
       .expect(200)
       .then(response =>
         response.body.command === false
-      )
-  })
-})
+      );
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,53 +1,53 @@
 require('dotenv-flow').config({
   silent: true
-})
+});
 
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET /servers/<uid> -> 200 | 404 | 5XX
 app.get('/servers/:serverID', (req, res) => {
-  const { serverID } = req.params
+  const { serverID } = req.params;
   res.json({
     action: 'confirm server existance',
     serverID
-  })
-})
+  });
+});
 // GET /servers/<uid>/users 200 | 403 | 404
 app.get('/servers/:serverID/users', (req, res) => {
-  const { serverID } = req.params
+  const { serverID } = req.params;
   res.json({
     action: 'list users',
     serverID
-  })
-})
+  });
+});
 // GET /servers/<uid>/users/<uid> 200 | 403 | 404
 app.get('/servers/:serverID/users/:userID', (req, res) => {
-  const { serverID, userID } = req.params
+  const { serverID, userID } = req.params;
   res.json({
     action: 'confirm user existance',
     serverID,
     userID
-  })
-})
+  });
+});
 // POST /servers/<uid>/users/<uid>/mute 200 | 403 | 404
 app.post('/servers/:serverID/users/:userID/:command', (req, res) => {
-  const { serverID, userID, command } = req.params
-  const validCommands = ['mute', 'unmute', 'deafen', 'undeafen']
+  const { serverID, userID, command } = req.params;
+  const validCommands = ['mute', 'unmute', 'deafen', 'undeafen'];
   res.json({
     serverID,
     userID,
     command: validCommands.includes(command) && command
-  })
-})
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-module.exports = app
+module.exports = app;
