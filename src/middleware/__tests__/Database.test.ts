0210d50b7de83224a0e23d57d93f1a0a6262aa0a
Rob Taylor <manix84@gmail.com>
2021-07-14 22:12:48 +0100

    Updating database tests to use promise.

--- src/middleware/__tests__/Database.test.ts
+++ src/middleware/__tests__/Database.test.ts
@@ -9,18 +9,18 @@ describe("Database", () => {
         connect: jest.fn(),
         query: jest.fn()
           .mockImplementation((_query, callback) => {
-            const error = new Error("mysql error");
+            const error: Error | null = new Error("MySQL Error");
             const results: string[] = [];
             const fields = ["some", "fields"];
             callback(error, results, fields);
           }),
         end: jest.fn()
       }))
-      .mockImplementationOnce(() => ({
+      .mockImplementation(() => ({
         connect: jest.fn(),
         query: jest.fn()
           .mockImplementation((_query, callback) => {
-            const error = null;
+            const error: Error | null = null;
             const results: string[] = [];
             const fields = ["some", "fields"];
             callback(error, results, fields);
@@ -34,39 +34,30 @@ describe("Database", () => {
     expect(dBase).toBeInstanceOf(Database);
   });
 
-  test("_connect", () => {
-    jest.mock("../Database");
-    const dBase = new Database();
-    dBase._connect();
-    expect(dBase._connect).toBeDefined();
-    expect(mysql.createConnection).toHaveBeenCalledTimes(1);
-  });
-
-  test("_runQuery", () => {
-    console.error = jest.fn();
-    const dBase = new Database();
-    expect(dBase._runQuery).toBeDefined();
-
-    dBase._runQuery("SELECT * FROM 'servers';", () => console.log);
-    dBase._runQuery("SELECT * FROM 'servers';");
-    expect(console.error).toHaveBeenCalledTimes(1);
-    expect(mysql.createConnection).toHaveBeenCalledTimes(2);
-  });
-
-  test("registerServer", () => {
+  test("registerServer", async () => {
     const dBase = new Database();
     expect(dBase.registerServer).toBeDefined();
-    dBase.registerServer(12345567890, "abcdefgHIJKLMN1234567", () => "");
-    dBase.registerServer(12345567890, "abcdefgHIJKLMN1234567", () => "", true);
-    expect(mysql.createConnection).toHaveBeenCalledTimes(2);
+    try {
+      await dBase.registerServer(12345567890, "abcdefgHIJKLMN1234567");
+    } catch (err) {
+      expect(err).toEqual(Error("MySQL Error"));
+    }
+    await dBase.registerServer(12345567890, "abcdefgHIJKLMN1234567");
+    await dBase.registerServer(12345567890, "abcdefgHIJKLMN1234567", true);
+    expect(mysql.createConnection).toHaveBeenCalledTimes(3);
   });
 
-  test("getServerID", () => {
+  test("getServerID", async () => {
     console.log = jest.fn();
     const dBase = new Database();
     expect(dBase.getServerID).toBeDefined();
-    dBase.getServerID("abcdefgHIJKLMN1234567");
-    expect(mysql.createConnection).toHaveBeenCalledTimes(1);
+    try {
+      await dBase.getServerID("abcdefgHIJKLMN1234567");
+    } catch (err) {
+      expect(err).toEqual(Error("MySQL Error"));
+    }
+    await dBase.getServerID("abcdefgHIJKLMN1234567");
+    expect(mysql.createConnection).toHaveBeenCalledTimes(2);
   });
 
 });
